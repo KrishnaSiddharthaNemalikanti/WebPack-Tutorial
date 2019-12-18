aa26ad96c4f2fdb45e0072de22434d6093a182bb
Ashwin Ramakrishnan <ashwinneo@gmail.com>
2019-12-18 09:53:11 +0530

    UI CHANGES

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,29 +1,112 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Input from "../presentational/Input.jsx";
+import Select from 'react-select';
+const axios = require('axios');
+var options = [];
 class FormContainer extends Component {
   constructor() {
     super();
     this.state = {
-      seo_title: ""
+      zip_code: "",
+      max_age: "",
+      animal: "",
+      response: [],
+      responseBreed: []
     };
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
     this.setState({ [event.target.id]: event.target.value });
   }
+  onSelectChanged(optionSelected) {
+    console.log(optionSelected)
+    let sql1 = "http://localhost:5000/getList?type=" + 'BREED' +"&animal="+optionSelected.value ;
+    axios.get(sql1).then(response => {
+      console.log(response)
+      this.setState(
+        {
+          responseBreed: response.data.data
+        }
+      )
+      console.log(this.state)
+  })
+  }
+  componentDidMount(){
+    let sql = "http://localhost:5000/getList?type=" + 'ANIMAL';
+    axios.get(sql).then(response => {
+      console.log(response)
+      this.setState(
+        {
+          response: response.data.data
+        }
+      )
+      console.log(this.state)
+  })
+  
+  }
   render() {
-    const { seo_title } = this.state;
+    const { max_age, zip_code, response,responseBreed } = this.state;
+    if(response.length>0){
+      for(var i=0;i<response.length; i++) {
+        var options = [{
+            value: response[i].animal,
+            label: response[i].animal
+          }];
+      }
+      
+    }
+    if(responseBreed.length>0){
+      var options1 = [];
+      for(var i=0;i<responseBreed.length; i++) {
+        var optionsBreed = {
+            value: responseBreed[i].breed,
+            label: responseBreed[i].breed
+          };
+          options1.push(optionsBreed);
+      }
+    }
     return (
       <form id="article-form">
         <Input
-          text="SEO title"
-          label="seo_title"
+          text="Maximum Age"
+          label="max_age"
           type="text"
-          id="seo_title"
-          value={seo_title}
+          id="max_age"
+          value={max_age}
           handleChange={this.handleChange}
         />
+        <Input
+          text="Zip Code"
+          label="zip_code"
+          type="text"
+          id="zip_code"
+          value={zip_code}
+          handleChange={this.handleChange}
+        />
+       <div>
+         <div><span>Animal</span></div>
+         <Select
+          name="form-field-name"
+          value={this.state.brandSelect}
+          options={options}
+          placeholder="Select a brand"
+          searchable={false}
+          onChange={e => this.onSelectChanged(e)}
+        />
+        </div>
+        
+        <div>
+         <div><span>Breed</span></div>
+         <Select
+          name="form-field-name"
+          value={this.state.brandSelect}
+          options={options1}
+          placeholder="Select a Breed"
+          searchable={false} 
+        />
+        </div>
+        <button>animal</button>
       </form>
     );
   }
